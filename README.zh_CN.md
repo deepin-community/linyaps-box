5a1f565f71624d38449ddbb25b8896db465c7c0e
dengbo11 <dengbo@uniontech.com>
2024-09-19 16:17:26 +0800

    chore: sync code
    sync code from OpenAtom-Linyaps repo
    
    Log:

--- README.zh_CN.md
+++ README.zh_CN.md
@@ -1,7 +1,5 @@
 # 玲珑：沙箱
 
-玲珑（Linglong） is the container application toolkit of deepin.
-
 玲珑是玲珑塔的简称，既表示容器能对应用有管控作用，也表明了应用/运行时/系统向塔一样分层的思想。
 
 ## Feature
---
+++ src/util/platform.h
@@ -0,0 +1,29 @@
+/*
+ * SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+#ifndef LINGLONG_BOX_SRC_UTIL_PLATFORM_H_
+#define LINGLONG_BOX_SRC_UTIL_PLATFORM_H_
+
+#include "3party/optional/optional.hpp"
+#include "common.h"
+
+namespace linglong {
+
+namespace util {
+
+int PlatformClone(int (*callback)(void *), int flags, void *arg, ...);
+
+int Exec(const util::str_vec &args, tl::optional<std::vector<std::string>> env_list);
+
+void Wait(const int pid);
+void WaitAll();
+void WaitAllUntil(const int pid);
+
+} // namespace util
+
+} // namespace linglong
+
+#endif /* LINGLONG_BOX_SRC_UTIL_PLATFORM_H_ */
